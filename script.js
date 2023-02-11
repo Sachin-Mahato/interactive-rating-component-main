388feffeb51d7d7447b966d375f5ce8e8cd93f25
Sachin <93040506+Sachin-Mahato@users.noreply.github.com>
2023-02-11 07:16:28 +0530

    Refractor Js code

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-const card = document.querySelector(".rating-card");
-const cardTwo = document.querySelector(".thank-you-card");
-const submit = document.querySelector("#submit")
-const btn = document.querySelectorAll(".rating-btn");
-const span = document.querySelector("#span-rating");
+// const card = document.querySelector(".rating-card");
+// const cardTwo = document.querySelector(".thank-you-card");
+// const submit = document.querySelector("#submit")
+// const btn = document.querySelectorAll(".rating-btn");
+// const span = document.querySelector("#span-rating");
 
-submit.addEventListener("click", function(){
+// submit.addEventListener("click", function(){
+//     cardTwo.classList.remove("thank-you-card");
+//     card.style.display = "none";
+// });
+
+// btn.forEach((rate) => {
+//     rate.addEventListener("click", () =>{
+//         span.innerHTML = rate.innerHTML;
+//     })
+// })
+
+// method 2 
+
+const card = document.querySelector(".rating-card");
+const submit = document.querySelector("#submit");
+const btns = document.querySelectorAll(".rating-btn");
+const span = document.querySelector("#span-rating");
+const cardTwo = document.querySelector(".thank-you-card");
+
+submit.addEventListener("click", () => {
     cardTwo.classList.remove("thank-you-card");
     card.style.display = "none";
 });
 
-btn.forEach((rate) => {
-    rate.addEventListener("click", () =>{
-        span.innerHTML = rate.innerHTML;
-    })
-})
+btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        span.textContent = btn.textContent;
+    });
+});
 
